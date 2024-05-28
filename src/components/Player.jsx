9291f317730da4737a474f16a02687e3971f36fc
tivereidoro <tivereidoro@yahoo.com>
2024-05-28 23:06:50 +0100

    Building player ui

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './playerStyle.css'
 import bgImage from "../assets/music.jpg"
 
 export default function Player() {
@@ -6,7 +7,7 @@ export default function Player() {
         <div className='player_container'>
             <img src={bgImage} alt='background image' className='bg_image' />
 
-            <div className='player_container'>
+            <div className='player_ui'>
                 <div></div>
             </div>
         </div>
