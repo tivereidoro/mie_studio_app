1a27394efaa828356a3e8603fe7274e5a6b5242f
tivereidoro <tivereidoro@yahoo.com>
2024-05-29 22:37:06 +0100

    Fix the routing challenge

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,36 @@
 import {
-  BrowserRouter,
+  createBrowserRouter,
+  RouterProvider
 } from "react-router-dom";
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+// import LoginPage from "./components/LoginPage";
+// import SignupPage from "./components/SignupPage";
+import Player from "./components/Player";
+import { Login } from "@mui/icons-material";
 import LoginPage from "./components/LoginPage";
 import SignupPage from "./components/SignupPage";
-import Player from "./components/Player";
 
 
 // Create routers for app navigation
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Player />
+  },
+  {
+    path: "login",
+    element: <LoginPage />
+  },
+  {
+    path: "signup",
+    element: <SignupPage />
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Player />
-      {/* <LoginPage />
-      <SignupPage /> */}
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
