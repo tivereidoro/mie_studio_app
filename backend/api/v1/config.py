93dda0f561ca2617ac95a4afd3f1ca43704befde
Ebuube <onwutaebubegideon1555@gmail.com>
2024-05-30 17:18:15 +0000

    Allowed cookies to be accessible

--- backend/api/v1/config.py
+++ backend/api/v1/config.py
@@ -7,13 +7,13 @@ class DevelopmentConfig:
     """Configuration for app in Development stage
     """
     DEBUG = True
-    SESSION_COOKIE_HTTPONLY = True
-    SESSION_COOKIE_SECURE = True
+    # SESSION_COOKIE_HTTPONLY = True
+    # SESSION_COOKIE_SECURE = True
 
 
 class ProductionConfig:
     """Configuration for app in production stage
     """
     DEBUG = False
-    SESSION_COOKIE_HTTPONLY = True
-    SESSION_COOKIE_SECURE = True
+    # SESSION_COOKIE_HTTPONLY = True
+    # SESSION_COOKIE_SECURE = True
