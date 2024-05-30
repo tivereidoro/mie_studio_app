1fa65d83c60b1226e8db7534a9b11c78d0063fda
tivereidoro <tivereidoro@yahoo.com>
2024-05-30 15:28:01 +0100

    All erros cleared

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -10,51 +10,51 @@ export default function Player() {
             <img src={bgImage} alt="bg image" className="bg_img" />
 
             <div className='player'>
-                <div class="player_body">
-                    <div class="body_cover">
-                        <ul class="list list_cover">
+                <div className="player_body">
+                    <div className="body_cover">
+                        <ul className="list list_cover">
                             <li>
-                                <a class="list_link" href=""><i class="bi bi-list"></i></a>
+                                <a className="list_link" href=""><i className="bi bi-list"></i></a>
                             </li>
 
                             <li>
-                                <a class="list_link" href=""></a>
+                                <a className="list_link" href=""></a>
                             </li>
 
                             <li>
-                                <a class="list_link" href=""><i class="bi bi-search"></i></a>
+                                <a className="list_link" href=""><i className="bi bi-search"></i></a>
                             </li>
                         </ul>
 
                         <img src={albumArt} alt="Album cover" />
 
-                        <div class="range"></div>
+                        <div className="range"></div>
                     </div>
 
-                    <div class="body_info">
-                        <div class="info_song">Song Title</div>
+                    <div className="body_info">
+                        <div className="info_song">Song Title</div>
 
-                        <div class="info_artist">Song Artist Name</div>
+                        <div className="info_artist">Song Artist Name</div>
                     </div>
 
-                    <div class="body_buttons">
-                        <ul class="list list_buttons">
-                            <li><a href="#" class="list_link"><i class="bi bi-skip-backward-fill"></i></a></li>
+                    <div className="body_buttons">
+                        <ul className="list list_buttons">
+                            <li><a href="#" className="list_link"><i className="bi bi-skip-backward-fill"></i></a></li>
 
-                            <li><a href="#" class="list_link"><i class="bi bi-play-circle"></i></a></li>
+                            <li><a href="#" className="list_link"><i className="bi bi-play-circle"></i></a></li>
 
-                            <li><a href="#" class="list_link"><i class="bi bi-skip-forward-fill"></i></a></li>
+                            <li><a href="#" className="list_link"><i className="bi bi-skip-forward-fill"></i></a></li>
                         </ul>
                     </div>
                 </div>
 
-                <div class="player_footer">
-                    <ul class="list list_footer">
-                        <li><a href="#" class="list_link"><i class="bi bi-heart"></i></a></li>
+                <div className="player_footer">
+                    <ul className="list list_footer">
+                        <li><a href="#" className="list_link"><i className="bi bi-heart"></i></a></li>
 
-                        <li><a href="#" class="list_link"><i class="bi bi-shuffle"></i></a></li>
+                        <li><a href="#" className="list_link"><i className="bi bi-shuffle"></i></a></li>
 
-                        <li><a href="#" class="list_link"><i class="bi bi-repeat"></i></a></li>
+                        <li><a href="#" className="list_link"><i className="bi bi-repeat"></i></a></li>
 
                     </ul>
                 </div>
